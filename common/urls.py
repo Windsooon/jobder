919ca9e91f8db62dbe6b19d3cdce8e6b59ba9d80
Windson <wiwindson@outlook.com>
2017-12-22 18:06:50 +0800

    update migrations

--- common/urls.py
+++ common/urls.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 from django.conf.urls import url
+from django.urls import path, re_path
 from common import views
 
 urlpatterns = [
-    url(r'^profile/$', views.profile, name='profile'),
     url(r'^settings/$', views.settings, name='settings'),
     url(r'^post-job/$', views.post_job, name='post_job'),
     url(r'^$', views.index),
+    re_path(
+        r'^github/(?P<name>([a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*){1,38})/$',
+        views.profile, name='profile'),
 ]
--- common/views.py
+++ common/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
 
 
 def index(request):
@@ -6,8 +8,9 @@ def index(request):
     return render(request, "index.html")
 
 
-def profile(request):
+def profile(request, name):
     """Profile page"""
+    user = get_object_or_404(get_user_model(), username=name)
     return render(request, "profile.html")
 
 
