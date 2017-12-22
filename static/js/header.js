0b4603757b378fa6b5a45ec9efac665ee33cdef7
Windson <wiwindson@outlook.com>
2017-12-22 11:55:16 +0800

    display user name directly

--- static/js/header.js
+++ static/js/header.js
@@ -1,38 +1,3 @@
-$(document).ready(function() {
-    const token = $("#api-token").val(); 
-    const url = "https://api.github.com/graphql"
-    get_user_name(url, token);
-});
-
-function update_name(name) {
-    $("#nav-user-name").text(name);
-    $("#nav-user-name").append("<span class='caret'></span>");
-}
-
-function get_user_name(url, token) {
-    var query = `query { viewer 
-        { login 
-          name
-        }}`
-
-   	$.ajax({
-		url: url,
-		type: "POST",
-		dataType: "json",
-        headers: {
-            Authorization: "bearer " + token,
-        },
-        data: JSON.stringify(
-            {
-                "query": query
-            }),
-		success:function(data){
-            data = data.data.viewer;
-            update_name(data.name);
-        }
-    }); 
-}
-
 document.addEventListener("DOMContentLoaded", function (event) {
     navbarFixedTopAnimation();
 });
