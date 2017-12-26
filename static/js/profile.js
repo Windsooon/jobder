e14dc2bc1409ad6fbcdf5b449b358143a2d6ef94
Windsooon <wiwindson@outlook.com>
2017-12-26 23:17:24 +0800

    update sta

--- static/js/profile.js
+++ static/js/profile.js
@@ -14,11 +14,13 @@ function get_user_data(url, token, user_name) {
                       createdAt
                       homepageUrl
                       nameWithOwner
-                      forkCount
+                      stargazers {
+                          totalCount
+                      }
                       projectsUrl
                       url
                       primaryLanguage {
-                        name 
+                          name 
                       }
                   }     
               }
@@ -30,9 +32,11 @@ function get_user_data(url, token, user_name) {
                       description
                       createdAt
                       homepageUrl
+                      url
                       nameWithOwner
-                      forkCount
-                      projectsUrl
+                      stargazers {
+                          totalCount
+                      }
                       url
                       primaryLanguage {
                         name 
@@ -57,11 +61,48 @@ function get_user_data(url, token, user_name) {
             data = data.data.user;
             update_profile(data);
             update_chart(data);
+            update_repo($(".repo-class"), data.repositories.edges);
+            update_repo($(".contributed-class"), data.repositoriesContributedTo.edges);
+            update_slider($('.repo-class'));
+            update_slider($('.contributed-class'));
         },
         error: function() {
             alert("You may reach rate limit, please try again later");
         }
     }); 
+}
+
+function update_repo(container, data) {
+    $.each(data, function(i, item) {
+        var $wrapper_div = $("<div />", {
+            "class": "wrapper-div",
+        });
+        var $wrapper_border = $("<div />", {
+            "class": "wrapper-border",
+        });
+        var $wrapper_span = $("<span />", {
+            "class": "wrapper-span",
+        });
+        var $inside_span = $("<span />", {
+            "class": "inside-span",
+        });
+        var $inside_a = $("<a />", {
+            "class": "inside-a",
+            "text": item.node.name,
+            "href": item.node.url,
+            "target": "_blank"
+        });
+        var $content_p = $("<p />", {
+            "class": "content-p",
+            "text": item.node.description || "None"
+        });
+        $wrapper_div.append($wrapper_border);
+        $wrapper_border.append($wrapper_span);
+        $wrapper_span.append($inside_span);
+        $inside_span.append($inside_a);
+        $wrapper_span.append($content_p);
+        container.append($wrapper_div);
+    });    
 }
 
 function update_profile(data) {
@@ -88,10 +129,11 @@ function get_fork_count(data) {
     var own_array = [0, 0, 0];
     var contribute_array = [0, 0, 0];
     $.each(data.repositories.edges, function(i, item) {
-        if (item.node.forkCount < 100) {
+        if (item.node.stargazers.totalCount < 100) {
+
             own_array[0] += 1;
         }
-        else if (item.node.forkCount < 500) {
+        else if (item.node.stargazers.totalCount < 500) {
             own_array[1] += 1;
         }
         else {
@@ -99,10 +141,10 @@ function get_fork_count(data) {
         }
     });
     $.each(data.repositoriesContributedTo.edges, function(i, item) {
-        if (item.node.forkCount < 100) {
+        if (item.node.stargazers.totalCount < 100) {
             contribute_array[0] += 1;
         }
-        else if (item.node.forkCount < 500) {
+        else if (item.node.stargazers.totalCount < 500) {
             contribute_array[1] += 1;
         }
         else {
@@ -202,10 +244,35 @@ function get_h_chart_config(data_list, type, suggest, color) {
     }
 };
 
-function addData(chart, label, data) {
-    chart.data.labels.push(label);
-    chart.data.datasets.forEach((dataset) => {
-        dataset.data.push(data);
+function update_slider(container) {
+    container.slick({
+        dots: true,
+        slidesPerRow: 3,
+        rows: 3,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          }
+        ]
     });
-    chart.update();
 }
