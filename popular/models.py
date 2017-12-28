4e48a80c0f0edb252a91d9233a44345210db3af6
Windson <wiwindson@outlook.com>
2017-12-28 19:01:47 +0800

    add popular app

---
+++ popular/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Popular(models.Model):
+    globle_id = models.CharField(max_length=256, unique=True)
+    name = models.CharField(max_length=256)
+    name_with_owner = models.CharField(max_length=256)
+    description = models.CharField(max_length=256, default="", null=True)
+    url = models.URLField(default="")
+    homepage_url = models.URLField(default="", null=True)
+    primary_language = models.CharField(max_length=256, default="", null=True)
+    start_count = models.IntegerField()
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
