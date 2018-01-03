d36b17709c8ab9099782ffa99cfe9ad0bf5f68c9
Windson <wiwindson@outlook.com>
2018-01-03 16:05:04 +0800

    clean code

--- static/js/header.js
+++ static/js/header.js
@@ -1,3 +1,4 @@
+const base_url = "https://127.0.0.1:8000/"
 document.addEventListener("DOMContentLoaded", function (event) {
     navbarFixedTopAnimation();
 });
---
+++ static/js/post_job.js
@@ -0,0 +1,43 @@
+$('#select-open-source').selectize({
+    valueField: 'title',
+    labelField: 'title',
+    searchField: 'title',
+    options: [],
+    create: false,
+    render: {
+        option: function(item, escape) {
+            var actors = [];
+            for (var i = 0, n = item.abridged_cast.length; i < n; i++) {
+                actors.push('<span>' + escape(item.abridged_cast[i].name) + '</span>');
+            }
+            return '<div>' +
+                '<img src="' + escape(item.posters.thumbnail) + '" alt="">' +
+                '<span class="title">' +
+                    '<span class="name">' + escape(item.title) + '</span>' +
+                '</span>' +
+                '<span class="description">' + escape(item.synopsis || 'No synopsis available at this time.') + '</span>' +
+                '<span class="actors">' + (actors.length ? 'Starring ' + actors.join(', ') : 'Actors unavailable') + '</span>' +
+            '</div>';
+        }
+    },
+    load: function(query, callback) {
+        if (!query.length) return callback();
+        $.ajax({
+            url: base_url + 'api/popular/',
+            type: 'GET',
+            dataType: 'json',
+            data: {
+                q: query,
+                page_limit: 10000,
+            },
+            error: function() {
+                callback();
+            },
+            success: function(res) {
+                console.log(res.movies);
+                callback(res.movies);
+            }
+        });
+    }
+});
+
