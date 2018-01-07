9ade0f08f9614833e402ba96d17aea2ed0003c25
Windsooon <wiwindson@outlook.com>
2018-01-07 20:41:49 +0800

    update post model

--- post/models.py
+++ post/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class Repo(models.Model):
@@ -12,6 +13,10 @@ class Post(models.Model):
     Remote = 1
     OnSite = 2
 
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='post')
     # job
     title = models.CharField(max_length=50)
     job_des = models.CharField(max_length=600)
