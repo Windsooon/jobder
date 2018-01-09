2aab2c171922573dfa8e319bb08ba310675af38f
Windsooon <wiwindson@outlook.com>
2018-01-10 00:25:57 +0800

    update models and delete popular app

--- post/models.py
+++ post/models.py
@@ -1,11 +1,31 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-
-class Repo(models.Model):
-    name = models.CharField(max_length=100)
+class Popular(models.Model):
+    globle_id = models.CharField(max_length=256, unique=True)
+    name = models.CharField(max_length=256)
+    name_with_owner = models.CharField(max_length=256)
+    description = models.CharField(max_length=256, blank=True)
+    url = models.URLField(blank=True)
+    homepage_url = models.URLField(blank=True)
+    primary_language = models.CharField(max_length=256, blank=True)
+    star_count = models.IntegerField()
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Repo(models.Model):
+    repo_id = models.IntegerField()
+    repo_name = models.CharField(max_length=256)
+    owner_name = models.CharField(max_length=256)
+    html_url = models.URLField(blank=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.owner_name + '/' + self.repo_name
 
 
 class Post(models.Model):
