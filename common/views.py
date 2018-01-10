81c50e3714ff1bde319c0a3c4815b8a4ff180e27
Windsooon <wiwindson@outlook.com>
2018-01-10 22:46:20 +0800

    update match

--- common/views.py
+++ common/views.py
@@ -1,11 +1,12 @@
 import json
+import base64
 import datetime
 import requests
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
 from allauth.socialaccount.models import SocialToken
-from post.models import Post
+from post.models import Post, Repo
 from jobs.set_logging import setup_logging
 from .query import get_repos_query
 
@@ -69,6 +70,9 @@ def job(request, id):
 
 
 def match(request):
+    '''
+    Find the most match jobs
+    '''
     try:
         social_token = SocialToken.objects.get(
             account__user__id=request.user.id)
@@ -80,4 +84,26 @@ def match(request):
     r = requests.post(
         'https://api.github.com/graphql',
         json.dumps({"query": query}), headers=headers)
-    logger.debug(r.content)
+    repo = [repo['node']['id'] for repo in r.json()['data']['user']['repositories']['edges']]
+    repo_contributedto = [repo['node']['id'] for repo in r.json()['data']['user']['repositoriesContributedTo']['edges']]
+    # repo contains a list of repo ids [14400303, 1404040]
+    repo.extend(repo_contributedto)
+
+    post_set = Post.objects.filter(pay=1).filter(
+        pay_time__gte=datetime.datetime.now()
+        -datetime.timedelta(days=60))
+
+    lst = []
+    for post in post_set:
+        dic = {'id': post.id, 'repos_lst': []}
+        for r in post.repo.all():
+            dic['repos_lst'].append(r.repo_id)
+        lst.append(dic)
+
+    finally_lst = []
+    for l in lst:
+        dic = {'id': l['id'], 'repos_len': 0}
+        dic['repos_len'] = len(set(repo) & set(l['repos_lst']))
+        finally_lst.append(dic) 
+
+    logger.debug(finally_lst)
