d44d1c6ae836a7d0fd49e56a5b5e288be45157af
Windson <wiwindson@outlook.com>
2018-01-12 11:16:47 +0800

    update test

--- tests/test_pages.py
+++ tests/test_pages.py
@@ -11,9 +11,20 @@ class PageTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Log in with Github')
 
-    def test_non_login_settings_404(self):
+    def test_pages_not_login_redirect(self):
+        expected_url = '/accounts/login/?next=/settings/'
         response = self.client.get(reverse('settings'))
-        self.assertContains(response, 'Please login to see this page.')
+        self.assertRedirects(
+            response, expected_url, status_code=302, target_status_code=200)
+
+        response = self.client.get(reverse('post_job'))
+        self.assertEqual(response.status_code, 302)
+
+        response = self.client.get(reverse('posted_jobs'))
+        self.assertEqual(response.status_code, 302)
+
+        response = self.client.get(reverse('match'))
+        self.assertEqual(response.status_code, 302)
 
     def test_user_login_frontpage_settings(self):
         user = create_one_account()
@@ -24,7 +35,13 @@ class PageTestCase(TestCase):
         response = self.client.get(reverse('settings'))
         self.assertContains(response, '1testoneaccount')
 
-    def test_user_profile(self):
+    def test_user_profile_without_login(self):
+        user = create_one_account()
+        response = self.client.get(
+            reverse('profile', kwargs={'name': user.username}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_profile_login(self):
         user = create_one_account()
         self.client.force_login(user)
         response = self.client.get(
