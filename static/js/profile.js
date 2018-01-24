26b09605ecd512f38d049f243320567495fe776d
Windsooon <wiwindson@outlook.com>
2018-01-24 22:55:50 +0800

    setup profile responsive

--- static/js/profile.js
+++ static/js/profile.js
@@ -295,24 +295,25 @@ function update_slider(container) {
           {
             breakpoint: 1024,
             settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
+              dots: true,
+              slidesPerRow: 3,
+              rows: 2,
             }
           },
           {
             breakpoint: 600,
             settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
+              dots: true,
+              slidesPerRow: 3,
+              rows: 2,
             }
           },
           {
             breakpoint: 480,
             settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
+              dots: true,
+              slidesPerRow: 2,
+              rows: 2,
             }
           }
         ]
