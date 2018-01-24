59545ce6fd24cc1889d0719e49ed25ebfaac802f
Windsooon <wiwindson@outlook.com>
2018-01-24 21:50:07 +0800

    update fabfile

--- fabfile.py
+++ fabfile.py
@@ -2,15 +2,16 @@ from fabric.contrib.files import exists, sed
 from fabric.api import env, local, run
 from fabric.operations import put
 
-REPO_URL = 'git@github.com:Windsooon/Unicooo-django.git'
 USER = 'Windson'
-TOUCH = '/var/www/www_jobder'
+TOUCH = '/var/www/www_jobder_net_wsgi.py'
+
+env.user = 'Windson'
+env.hosts = ['ssh.pythonanywhere.com']
+
 
 
 def deploy():
     site_folder = '/home/{0}/jobder/'.format(USER)
     # git stath
-    run('cd {0} && git stash'.format(site_folder))
-    run('git pull origin master')
-    run('git stash pop')
-    run('touch {0}'.format(TOUCH))
+    run(
+        'cd {0} && git stash && git pull origin master && git stash pop && touch {1}'.format(site_folder, TOUCH))
