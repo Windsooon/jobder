83ef79f4442c0e69c5ae98fa24184e8aa6eee03c
Windsooon <wiwindson@outlook.com>
2018-02-06 23:43:04 +0800

    update fab

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,5 @@
 import os
 from fabric.api import env, run
-from fabric.network import ssh
 
 USER = 'Windson'
 TOUCH = '/var/www/www_jobder_net_wsgi.py'
@@ -12,15 +11,24 @@ site_folder = '/home/{0}/jobder/'.format(USER)
 
 def deploy():
     # git stath
-    # run_deploy()
-    update_key()
+    run_deploy()
+    update_val()
+    reset()
 
 
-def update_key():
+def update_val():
     js_key_path = os.path.join(site_folder, 'static/js/job.js')
+    js_base_url = os.path.join(site_folder, 'static/js/base.js')
+    py_key_path = os.path.join(site_folder, 'common/const.py')
     run('sed -i "s/pk_test_fEJG3FbEEKCGhriUfqjWJZG5/pk_live_1vr5cqWQbuK89pF5nDwxNXCQ/g" {0}'.format(js_key_path))
+    run('sed -i "s/sk_test_Mrp9fWK53zgna3gSbGGUy60W/sk_live_hdOhJFb3UdlQhvFlW9LfWAaE/g" {0}'.format(py_key_path))
+    run('sed -i "s=http://127.0.0.1:8000/=https://www.jobder.net/=g" {0}'.format(js_base_url))
+    run('sed -i "s/DEBUG\s=\sTrue/DEBUG\s=\sFalse/g" {0}'.format(js_base_url))
 
 
 def run_deploy():
-    run(
-        'cd {0} && git stash && git pull origin master && git stash pop && touch {1}'.format(site_folder, TOUCH))
+    run('cd {0} && git stash && git pull origin master && git stash pop && source ~/.virtualenvs/jobder/bin/activate && python manage.py migrate'.format(site_folder))
+
+
+def reset():
+    run('touch /var/www/www_jobder_net_wsgi.py')
