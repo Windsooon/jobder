02e2d6b695866805510e8fafdee175fa615eaa8f
Windson <wiwindson@outlook.com>
2018-02-09 10:51:11 +0800

    update settings

--- fabfile.py
+++ fabfile.py
@@ -10,7 +10,6 @@ site_folder = '/home/{0}/jobder/'.format(USER)
 
 
 def deploy():
-    # git stath
     run_deploy()
     update_val()
     reset()
@@ -20,10 +19,12 @@ def update_val():
     js_key_path = os.path.join(site_folder, 'static/js/job.js')
     js_base_url = os.path.join(site_folder, 'static/js/base.js')
     py_key_path = os.path.join(site_folder, 'common/const.py')
+    settings_path = os.path.join(site_folder, 'jobs/settings.py')
     run('sed -i "s/pk_test_fEJG3FbEEKCGhriUfqjWJZG5/pk_live_1vr5cqWQbuK89pF5nDwxNXCQ/g" {0}'.format(js_key_path))
     run('sed -i "s/sk_test_Mrp9fWK53zgna3gSbGGUy60W/sk_live_hdOhJFb3UdlQhvFlW9LfWAaE/g" {0}'.format(py_key_path))
     run('sed -i "s=http://127.0.0.1:8000/=https://www.jobder.net/=g" {0}'.format(js_base_url))
     run('sed -i "s/DEBUG\s=\sTrue/DEBUG\s=\sFalse/g" {0}'.format(js_base_url))
+    run('sed -i "s/[\'*\']/[\'www.jobder.net\']/g" {0}'.format(settings_path))
 
 
 def run_deploy():
