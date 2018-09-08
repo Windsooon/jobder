1b68cc8cdab2ec397267d700c1eacc58e294db6c
Windson yang <wiwindson@outlook.com>
2018-09-08 10:02:47 +0800

    udpate token to newest token

--- common/const.py
+++ common/const.py
@@ -7,5 +7,6 @@ PROFILE = 'Profile'
 RANDOM = 'Jobs List'
 STRIPE_API_KEY = 'sk_test_Mrp9fWK53zgna3gSbGGUy60W'
 TOKEN_LIST = [
-    '336d83e0a59214fd5a5e26e2fbe6ad8a3dd0beea',
+    'e1170ae5def9ea167f2e641199d5a96f008c9c4c',
+    '88837750af80a2e4cac7bbb4ca8cb74d3ee25cbd',
     ]
