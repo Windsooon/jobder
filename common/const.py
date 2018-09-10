a7e208283f867466acc384aea94c6d243c66e514
Windson yang <wiwindson@outlook.com>
2018-09-10 19:26:03 +0800

    fix const bug

--- common/const.py
+++ common/const.py
@@ -1,4 +1,4 @@
-from tokens import TOKEN_LIST
+from common import tokens
 
 MATCH = 'Match'
 LOGIN = 'Log in with GitHub'
@@ -8,3 +8,4 @@ PROFILE = 'Profile'
 SEARCH = 'Search Contributors'
 JOBLIST = 'Jobs List'
 STRIPE_API_KEY = 'sk_test_Mrp9fWK53zgna3gSbGGUy60W'
+TOKEN_LIST = tokens.TOKEN_LIST
--- tests/test_pages.py
+++ tests/test_pages.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from django.utils import timezone
 from django.urls import reverse
 from tests.base import create_one_account, create_one_job
-from common.const import FIND, BROWSE, RANDOM
+from common.const import MATCH, LOGIN, JOBLIST, SEARCH
 from post.models import Post
 from tests.base import GITHUB_REPO_RETURN
 
@@ -14,24 +14,12 @@ class PageTestCase(TestCase):
     def test_front_page_200_with_without_login(self):
         response = self.client.get(reverse('front_page'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, BROWSE)
+        self.assertContains(response, LOGIN)
         user = create_one_account()
         self.client.force_login(user)
         response = self.client.get(reverse('front_page'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, FIND)
-        self.assertContains(response, BROWSE)
-
-    def test_explain_page(self):
-        response = self.client.get(reverse('explain'))
-        self.assertContains(response, 'Why built Open Source Jobs')
-        self.assertEqual(response.status_code, 200)
-
-    def test_browse_no_post(self):
-        user = create_one_account()
-        self.client.force_login(user)
-        response = self.client.get(reverse('browse'))
-        self.assertEqual(response.status_code, 404)
+        self.assertContains(response, MATCH)
 
     def test_browse_page_200_with_without_login(self):
         user = create_one_account()
@@ -50,42 +38,48 @@ class PageTestCase(TestCase):
             apply='https://job.example.com',
             pay=1,
             pay_time=timezone.now())
+        response = self.client.get(reverse('front_page'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Jobs Created')
         self.client.logout()
         response = self.client.get(reverse('browse'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, RANDOM)
+        self.assertContains(response, 'Senior Software Engineer')
+
+    def test_explain_page(self):
+        response = self.client.get(reverse('explain'))
+        self.assertContains(response, 'Why you built Open Source Jobs')
+        self.assertContains(response, 'Q & A')
+        self.assertEqual(response.status_code, 200)
+
+    def test_browse_no_post(self):
+        user = create_one_account()
+        self.client.force_login(user)
+        response = self.client.get(reverse('browse'))
+        self.assertContains(response, JOBLIST)
+        self.assertContains(response, 'Show Remote Jobs')
+        self.assertEqual(response.status_code, 200)
 
     def test_other_pages_not_login_redirect(self):
+        # Before user login
         response = self.client.get(reverse('post_job'))
         self.assertEqual(response.status_code, 302)
         response = self.client.get(reverse('token'))
         self.assertEqual(response.status_code, 302)
         response = self.client.get(reverse('repo_search'))
         self.assertEqual(response.status_code, 302)
-        response = self.client.get(reverse('posted_jobs'))
-        self.assertEqual(response.status_code, 302)
-        response = self.client.get(reverse('repo_search'))
-        self.assertEqual(response.status_code, 302)
         response = self.client.get(reverse('contributors'))
         self.assertEqual(response.status_code, 302)
         response = self.client.get(reverse('posted_jobs'))
         self.assertEqual(response.status_code, 302)
-
-    def test_contributors_login(self):
+        # After user login
         user = create_one_account()
         self.client.force_login(user)
         response = self.client.get(reverse('contributors'))
         self.assertEqual(response.status_code, 200)
-
-    def test_posted_job_login(self):
-        user = create_one_account()
-        self.client.force_login(user)
         response = self.client.get(reverse('posted_jobs'))
         self.assertEqual(response.status_code, 200)
-
-    def test_post_job_pages(self):
-        user = create_one_account()
-        self.client.force_login(user)
+        self.assertContains(response, SEARCH)
         response = self.client.get(
             reverse('post_job'))
         self.assertEqual(response.status_code, 200)
@@ -107,9 +101,8 @@ class PageTestCase(TestCase):
         self.post3 = create_one_job(
             self.user.id, title='Data Scientist', pay=True, repo_id=2)
         repos.return_value = Repo
-        response = self.client.get(
-            reverse('match', kwargs={'name': self.user.username}))
-        self.assertContains(response, '<a class="job-title" target="_blank"')
+        response = self.client.get(reverse('match'))
+        self.assertContains(response, '<a class="job-title"')
         self.assertContains(response, 'Senior Software Engineer')
         self.assertContains(
             response, '<i class="fa fa-building building"')
@@ -132,16 +125,12 @@ class PageTestCase(TestCase):
         self.post4 = create_one_job(
             self.user.id, title='Frontend Engineer', pay=True, repo_id=1)
         repos.return_value = Repo
-        response = self.client.get('/match/Windsooon/?type=remote')
-        self.assertContains(response, '<a class="job-title" target="_blank"')
+        response = self.client.get('/match/?type=remote')
+        self.assertContains(response, '<a class="job-title"')
         self.assertNotContains(response, 'Senior Software Engineer')
         self.assertContains(response, 'Backend Engineer')
         self.assertContains(
             response, '<i class="fa fa-building building"')
-
-    def test_repo_search_miss_id_without_login(self):
-        response = self.client.get(reverse('repo_search'))
-        self.assertEqual(response.status_code, 302)
 
     def test_repo_search_miss_id(self):
         user = create_one_account()
@@ -166,15 +155,15 @@ class PageTestCase(TestCase):
         user = create_one_account()
         self.client.force_login(user)
         create_one_job(user.id, pay=True)
-        response = self.client.get(
-            reverse('match', kwargs={'name': 'Windsooon'}))
+        response = self.client.get(reverse('match'))
         response = self.client.get(
             "%s?repo_id=4164482" % (reverse('repo_search')))
         user_response = {
             'length': 1,
             'data': [{
-                'username': 'Windsooon',
-                'avatar_url': 'https://avatars2.githubusercontent.com/u/14333046?v=4'}
+                'username': '1testoneaccount',
+                'avatar_url':
+                    'https://avatars2.githubusercontent.com/u/14333046?v=4'}
             ]
         }
         self.assertEqual(response.json(), user_response)
