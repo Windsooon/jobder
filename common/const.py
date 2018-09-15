1aa7653c2c8946422ddf11ee99e4d18a66f53f7a
Windson yang <wiwindson@outlook.com>
2018-09-15 14:04:57 +0800

    add pub key and plan id

--- common/const.py
+++ common/const.py
@@ -8,4 +8,6 @@ PROFILE = 'Profile'
 SEARCH = 'Search Contributors'
 JOBLIST = 'Jobs List'
 STRIPE_API_KEY = 'sk_test_Mrp9fWK53zgna3gSbGGUy60W'
+STRIPE_PUB_KEY = 'pk_test_fEJG3FbEEKCGhriUfqjWJZG5'
+PLAN_ID = 'plan_DbQUUDQ7Htlo7H'
 TOKEN_LIST = tokens.TOKEN_LIST
